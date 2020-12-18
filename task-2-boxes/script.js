314d64f77bb672d0d4675c1cf2c6fa4364b1ef9a
ashish raj <mr.ashishrajyadav@gmail.com>
2020-12-18 11:00:51 +0530

    task-2 completed

---
+++ task-2-boxes/script.js
@@ -0,0 +1,13 @@
+
+
+function changeIndex(i,id){
+    var box = document.getElementById(id.id)
+    if(box.style.zIndex==1){
+        box.style.zIndex=0
+    }
+    else{
+        
+        box.style.zIndex=1       
+    } 
+    
+}
